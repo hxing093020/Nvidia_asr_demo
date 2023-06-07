87c972d7f36b63f6df45f83aaef9389687ed288a
Hua Xing <xinghua.utd@gmail.com>
2023-06-07 10:21:15 -0700

    second commit

--- demo.py
+++ demo.py
@@ -10,7 +10,10 @@ wavFiles.append('affed74c3b93602385f6d52ba9076dee.wav')
 wavFiles.append('cadc501bf659b6c487aa6f692552e317.wav')
 wavFiles.append('f50a5868f19fed8e867dd3983ff784d4.wav')
 wavFiles.append('sample4.wav')
-wavFiles.append('sample7.wav')
+wavFiles.append('sample8.wav')
+wavFiles.append('sample3.wav')
+wavFiles.append('sample5.wav')
+wavFiles.append('sample1.wav')
 
 GT = []
 GT.append('the vehicle does start and drive')
@@ -19,7 +22,10 @@ GT.append('today only about eleven percent of federal it systems are running in 
 GT.append('even nuclear weapons are only about one percent efficient chemical weapons are a tiny fraction of a percent in terms of their efficiency')
 GT.append('we\'re gonna come over here and there it is scale')
 GT.append('would you like cream and sugar')
-GT.append('the hot and spicy mcchickens')
+GT.append('um can i just get three mcchickens')
+GT.append('okay and what\'s the code')
+GT.append('uh no not just regular')
+GT.append('twenty piece chicken nuggets')
 
 PT = []
 PT.append(':red[it\'ll be a good bus store] and drive')
@@ -27,8 +33,11 @@ PT.append("always forgive :red[their] enemies nothing annoys them so much")
 PT.append(':red[to day] only about eleven per cent of federal it :red[t] systems are running in the cloud')
 PT.append('even nuclear weapons are only about :red[oneer] efficient chemical weapons are a tiny fraction of a :red[perc] in terms of their efficiency')
 PT.append('we\'re :red[going to] come over here and there it is scale')
-PT.append('would you like :red[frem chergen]')
 PT.append('the :red[gds why you make sure]')
+PT.append('um can i just get three :red[make check ins]')
+PT.append('okay and :red[wolstter] code')
+PT.append('uh no :red[no that\'s very go]')
+PT.append(':red[pony peace] chicken nuggets')
 
 
 FT = []
@@ -38,7 +47,11 @@ FT.append('today only about eleven percent of federal it systems are running in 
 FT.append('even nuclear weapons are only about one percent efficient chemical weapons are a tiny fraction of a percent in terms of their efficiency')
 FT.append('we\'re gonna come over here and there it is scale')
 FT.append('would you like cream and sugar')
-FT.append('the high and spicy mchchickens')
+FT.append(':red[(um)] can i just get three mcchickens')
+FT.append('okay and what\'s the code')
+FT.append(':red[(uh)] no not just regular')
+FT.append('twenty piece chicken nuggets')
+
 st.set_page_config(layout='wide')
 col1, col2, col3, col4 = st.columns(4)
 
